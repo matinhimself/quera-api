9fd2bf8346879129003c23ccb174e99696d8db09
xHossein <77378345+xHossein@users.noreply.github.com>
2021-11-20 12:46:09 +0330

    remove unused class

--- querapi/__init__.py
+++ querapi/__init__.py
@@ -1,13 +1,12 @@
 from querapi.mixins import AuthMixin, ContestMixin, ClassMixin, CourseMixin
 
 
-class Client:
-    def __init__(self):
-        self.name: str
-        self.email: str
-
-
-class QueraApi(AuthMixin, ContestMixin,
-               ClassMixin, CourseMixin):
+class Client(
+            AuthMixin,
+            ContestMixin,
+            ClassMixin,
+            CourseMixin  
+        ):
+    
     def __init__(self):
         super().__init__()
