4b536eb6203cb77e1f77b65bc65b30adfeedc798
xHossein <77378345+xHossein@users.noreply.github.com>
2021-11-20 13:04:53 +0330

    add save and load session

--- querapi/__init__.py
+++ querapi/__init__.py
@@ -1,3 +1,4 @@
+import json
 from querapi.mixins import AuthMixin, ContestMixin, ClassMixin, CourseMixin
 
 
@@ -10,3 +11,13 @@ class Client(
     
     def __init__(self):
         super().__init__()
+
+    def save_session(self, path: str) -> bool:
+        with open(path, 'w') as wf:
+            json.dump(self.session.cookies.get_dict(), wf, indent=4)
+        return True
+    
+    def load_session(self, path: str) -> bool:
+        with open(path, 'r') as rf:
+            self.login_by_session_id(json.load(rf)['session_id'])
+        return True
